3142d785069e423f2fdb34e5719648cd2446fff6
Shuhao Cao <scao.math@gmail.com>
2020-10-01 10:42:02 -0500

    Update a tf model

--- dataset_vis.py
+++ dataset_vis.py
@@ -7,7 +7,8 @@ import seaborn as sns
 import forgi.graph.bulge_graph as fgb
 import forgi.visual.mplotlib as fvm
 
-plt.rcParams['figure.figsize'] = (10, 7);
+# plt.rcParams['figure.figsize'] = (8, 7);
+# plt.rcParams['axes.facecolor'] = 'gray'
 #%%
 root_dir = './data/'
 train_path = os.path.join(root_dir, 'train.json')
@@ -15,6 +16,7 @@ test_path = os.path.join(root_dir, 'test.json')
 bpps_dir = os.path.join(root_dir, 'bpps')
 
 train = pd.read_json(train_path, lines=True)
+train = train[train.signal_to_noise > 1].reset_index(drop = True)
 test = pd.read_json(test_path, lines=True)
 train.head(3)
 # %%
@@ -23,13 +25,15 @@ for c in train.columns:
         print(c, ':', train[c].apply(lambda x: len(x))[0])
 # %%
 def plot_sample(idx = 1):
+    fig = plt.figure(figsize=(15,15))
+    fig.patch.set_facecolor((0.75,0.75,0.8))
     samp = train[train.index== idx]
     rna = []
     seq = samp.loc[samp.index[0], 'sequence']
     struct = samp.loc[samp.index[0], 'structure']
     bg = fgb.BulgeGraph.from_fasta_text(f'>rna1\n{struct}\n{seq}')[0]
-    fvm.plot_rna(bg)
+    fvm.plot_rna(bg, text_kwargs={"fontweight":"black"}, lighten=0.8,backbone_kwargs={"linewidth":3})
     plt.show()
 # %%
-plot_sample(2)
+plot_sample(1)
 # %%
--- test_ae_gnn_cnn.py
+++ test_ae_gnn_cnn.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 
 # %%
 pretrain_dir = None # model dir for resuming training. if None, train from scrach
-
+DEBUG = True
 one_fold = False # if True, train model at only first fold. use if you try a new idea quickly.
 run_test = True # if True, use small data. you can check whether this code run or not
 denoise = True # if True, use train data whose signal_to_noise > 1
@@ -120,9 +120,8 @@ Ss = get_structure_adj(train)
 Ss_pub = get_structure_adj(test_pub)
 Ss_pri = get_structure_adj(test_pri)
 
-
-#%% explicit un-enscapuslated routine of get_structure_adj
-i=1
+#%% explicit un-enscapuslated routine of get_structure_adj for id #1
+i = 1
 seq_length = train["seq_length"].iloc[i]
 structure = train["structure"].iloc[i]
 sequence = train["sequence"].iloc[i]
@@ -146,6 +145,15 @@ for i in tqdm(range(seq_length)):
         a_structures[(sequence[i], sequence[start])][i, start] = 1
 a_strc = np.stack([a for a in a_structures.values()], axis = 2)
 a_strc = np.sum(a_strc, axis = 2, keepdims = True)
+
+#%% base pairing prob plot vs structure
+fig, axes = plt.subplots(2, 2, figsize=(10,10))
+axes = axes.flatten()
+a_strc = a_strc.squeeze()
+axes[0].spy(a_strc)
+axes[1].spy(As[1])
+axes[2].spy(As[1]*(As[1]>1e-1))
+axes[3].spy(As[1]*(a_strc>0))
 
 # %% visualize the neighboring struct in train
 
@@ -195,6 +203,272 @@ As_pub = np.concatenate([As_pub[:,:,:,None], Ss_pub, Ds_pub], axis = 3).astype(n
 As_pri = np.concatenate([As_pri[:,:,:,None], Ss_pri, Ds_pri], axis = 3).astype(np.float32)
 # del Ss, Ds, Ss_pub, Ds_pub, Ss_pri, Ds_pri
 As.shape, As_pub.shape, As_pri.shape
+
+#%%
+## sequence
+def return_ohe(n, i):
+    tmp = [0] * n
+    tmp[i] = 1
+    return tmp
+
+def get_input(train):
+    '''
+    get node features, which is one hot encoded
+    S: paired "Stem"
+    M: Multiloop
+    I: Internal loop
+    B: Bulge
+    H: Hairpin loop
+    E: dangling End
+    X: eXternal loop
+    '''
+    mapping = {}
+    vocab = ["A", "G", "C", "U"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_node = np.stack(train["sequence"].apply(lambda x : list(map(lambda y : mapping[y], list(x)))))
+
+    mapping = {}
+    vocab = ["S", "M", "I", "B", "H", "E", "X"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_loop = np.stack(train["predicted_loop_type"].apply(lambda x : list(map(lambda y : mapping[y], list(x)))))
+    
+    mapping = {}
+    vocab = [".", "(", ")"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_structure = np.stack(train["structure"].apply(lambda x : list(map(lambda y : mapping[y], list(x)))))
+    
+    
+    X_node = np.concatenate([X_node, X_loop], axis = 2)
+    
+    ## interaction
+    a = np.sum(X_node * (2 ** np.arange(X_node.shape[2])[None, None, :]), axis = 2)
+    vocab = sorted(set(a.flatten()))
+    print(vocab, '\n')
+    ohes = []
+    for v in vocab:
+        ohes.append(a == v)
+    ohes = np.stack(ohes, axis = 2)
+    X_node = np.concatenate([X_node, ohes], axis = 2).astype(np.float32)
+    print(X_node.shape, '\n')
+    return X_node
+
+X_node = get_input(train)
+X_node_pub = get_input(test_pub)
+X_node_pri = get_input(test_pri)
+
+
+#%% ohe of A G C U base
+if DEBUG:
+    mapping = {}
+    vocab = ["A", "G", "C", "U"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_node = np.stack(train["sequence"].apply(lambda x : list(map(lambda y : mapping[y], list(x)))))
+
+# ohe of loop
+    mapping = {}
+    vocab = ["S", "M", "I", "B", "H", "E", "X"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_loop = np.stack(train["predicted_loop_type"].apply(lambda x : 
+                                                list(map(lambda y : 
+                                                        mapping[y], list(x)))))
+    X_node = np.concatenate([X_node, X_loop], axis = 2)
+    # This is not used
+    mapping = {}
+    vocab = [".", "(", ")"]
+    for i, s in enumerate(vocab):
+        mapping[s] = return_ohe(len(vocab), i)
+    X_structure = np.stack(train["structure"].apply(lambda x : list(map(lambda y : mapping[y], list(x)))))
+#%% this ohe may not be needed
+if DEBUG:
+    # obtain the number of types of nodes considering all nodal features (Base type x loop type)
+    a = np.sum(X_node * (2 ** np.arange(X_node.shape[2])[None, None, :]), axis = 2)
+    vocab = sorted(set(a.flatten()))
+    print(vocab, '\n')
+    ohes = []
+    for v in vocab:
+        ohes.append(a == v)
+    ohes = np.stack(ohes, axis = 2)
+
+
+#%% model
+import tensorflow as tf
+from tensorflow.keras import layers as L
+# import tensorflow_addons as tfa 
+from tensorflow.keras import backend as K
+
+def mcrmse(t, p, seq_len_target = seq_len_target):
+    ## calculate mcrmse score by using numpy
+    t = t[:, :seq_len_target]
+    p = p[:, :seq_len_target]
+    
+    score = np.mean(np.sqrt(np.mean(np.mean((p - t) ** 2, axis = 1), axis = 0)))
+    return score
+
+def mcrmse_loss(y_target, y_pred, seq_len_target = seq_len_target):
+    ## calculate mcrmse score by using tf
+    y_target = y_target[:, :seq_len_target]
+    y_pred = y_pred[:, :seq_len_target]
+    
+    loss = tf.reduce_mean(tf.sqrt(tf.reduce_mean(tf.reduce_mean((y_target - y_pred) ** 2, axis = 1), axis = 0)))
+    loss += 2e-1*tf.reduce_mean(tf.sqrt(tf.reduce_mean(tf.reduce_mean(tf.abs(y_target - y_pred), axis = 1), axis = 0)))
+    return loss
+
+def attention(x_inner, x_outer, n_factor, dropout):
+    x_Q =  L.Conv1D(n_factor, 1, activation='linear', 
+                  kernel_initializer='glorot_uniform',
+                  bias_initializer='glorot_uniform',
+                 )(x_inner)
+    x_K =  L.Conv1D(n_factor, 1, activation='linear', 
+                  kernel_initializer='glorot_uniform',
+                  bias_initializer='glorot_uniform',
+                 )(x_outer)
+    x_V =  L.Conv1D(n_factor, 1, activation='linear', 
+                  kernel_initializer='glorot_uniform',
+                  bias_initializer='glorot_uniform',
+                 )(x_outer)
+    x_KT = L.Permute((2, 1))(x_K)
+    res = L.Lambda(lambda c: K.batch_dot(c[0], c[1]) / np.sqrt(n_factor))([x_Q, x_KT])
+#     res = tf.expand_dims(res, axis = 3)
+#     res = L.Conv2D(16, 3, 1, padding = "same", activation = "relu")(res)
+#     res = L.Conv2D(1, 3, 1, padding = "same", activation = "relu")(res)
+#     res = tf.squeeze(res, axis = 3)
+    att = L.Lambda(lambda c: K.softmax(c, axis=-1))(res)
+    att = L.Lambda(lambda c: K.batch_dot(c[0], c[1]))([att, x_V])
+    return att
+
+def multi_head_attention(x, y, n_factor, n_head, dropout):
+    if n_head == 1:
+        att = attention(x, y, n_factor, dropout)
+    else:
+        n_factor_head = n_factor // n_head
+        heads = [attention(x, y, n_factor_head, dropout) for i in range(n_head)]
+        att = L.Concatenate()(heads)
+        att = L.Dense(n_factor, 
+                      kernel_initializer='glorot_uniform',
+                      bias_initializer='glorot_uniform',
+                     )(att)
+    x = L.Add()([x, att])
+    x = L.LayerNormalization()(x)
+    if dropout > 0:
+        x = L.Dropout(dropout)(x)
+    return x
+
+def res(x, unit, kernel = 5, rate = 0.1):
+    h = L.Conv1D(unit, kernel, 1, padding = "same", activation = None)(x)
+    h = L.LayerNormalization()(h)
+    h = L.LeakyReLU()(h)
+    h = L.Dropout(rate)(h)
+    return L.Add()([x, h])
+
+def forward(x, unit, kernel = 5, rate = 0.1):
+#     h = L.Dense(unit, None)(x)
+    h = L.Conv1D(unit, kernel, 1, padding = "same", activation = None)(x)
+    h = L.LayerNormalization()(h)
+    h = L.Dropout(rate)(h)
+#         h = tf.keras.activations.swish(h)
+    h = L.LeakyReLU()(h)
+    h = res(h, unit, kernel, rate)
+    return h
+
+def adj_attn(x, adj, unit, n = 2, rate = 0.1):
+    x_a = x
+    x_as = []
+    for i in range(n):
+        x_a = forward(x_a, unit)
+        x_a = tf.matmul(adj, x_a) ## aggregate neighborhoods
+        x_as.append(x_a)
+    if n == 1:
+        x_a = x_as[0]
+    else:
+        x_a = L.Concatenate()(x_as)
+    x_a = forward(x_a, unit)
+    return x_a
+
+
+def get_base(config):
+    ## base model architecture 
+    ## node, adj -> middle feature
+    
+    node = tf.keras.Input(shape = (None, X_node.shape[2]), name = "node")
+    adj = tf.keras.Input(shape = (None, None, As.shape[3]), name = "adj")
+    
+    adj_learned = L.Dense(1, "relu")(adj)
+    adj_all = L.Concatenate(axis = 3)([adj, adj_learned])
+        
+    xs = []
+    xs.append(node)
+    x1 = forward(node, 128, kernel = 3, rate = 0.1)
+    x2 = forward(x1, 64, kernel = 6, rate = 0.1)
+    x3 = forward(x2, 32, kernel = 15, rate = 0.1)
+    x4 = forward(x3, 16, kernel = 30, rate = 0.1)
+    x = L.Concatenate()([x1, x2, x3, x4])
+    
+    for unit in [64, 32]:
+        x_as = []
+        for i in range(adj_all.shape[3]):
+            x_a = adj_attn(x, adj_all[:, :, :, i], unit, rate = 0.0)
+            x_as.append(x_a)
+        x_c = forward(x, unit, kernel = 30)
+        
+        x = L.Concatenate()(x_as + [x_c])
+        x = forward(x, unit)
+        x = multi_head_attention(x, x, unit, 4, 0.0)
+        xs.append(x)
+        
+    x = L.Concatenate()(xs)
+
+    model = tf.keras.Model(inputs = [node, adj], outputs = [x])
+    return model
+
+
+def get_ae_model(base, config):
+    ## denoising auto encoder part
+    ## node, adj -> middle feature -> node
+    
+    node = tf.keras.Input(shape = (None, X_node.shape[2]), name = "node")
+    adj = tf.keras.Input(shape = (None, None, As.shape[3]), name = "adj")
+
+    x = base([L.SpatialDropout1D(0.4)(node), adj])
+    x = forward(x, 64, rate = 0.3)
+    p = L.Dense(X_node.shape[2], "sigmoid")(x)
+    
+#     loss = - tf.reduce_mean(40 * node * tf.math.log(p + 1e-6) + (1 - node) * tf.math.log(1 - p + 1e-6))
+    loss = - tf.reduce_mean(node * tf.math.log(p + 1e-6) + (1 - node) * tf.math.log(1 - p + 1e-6))
+    model = tf.keras.Model(inputs = [node, adj], outputs = [loss])
+    
+    opt = get_optimizer()
+    model.compile(optimizer = opt, loss = lambda t, y : y)
+    return model
+
+
+def get_model(base, config):
+    ## regression part
+    ## node, adj -> middle feature -> prediction of targets
+    
+    node = tf.keras.Input(shape = (None, X_node.shape[2]), name = "node")
+    adj = tf.keras.Input(shape = (None, None, As.shape[3]), name = "adj")
+    
+    x = base([node, adj])
+    x = forward(x, 128, rate = 0.3)
+    x = forward(x, 64, rate = 0.1)
+    x = L.Dense(5, None)(x)
+
+    model = tf.keras.Model(inputs = [node, adj], outputs = [x])
+    
+    opt = get_optimizer()
+    model.compile(optimizer = opt, loss = mcrmse_loss)
+    return model
+
+def get_optimizer():
+#     opt = tf.keras.optimizers.SGD(0.05, momentum = 0.9, nesterov=True)
+    opt = tf.optimizers.Adam()
+    return opt
+
 # %%
 if __name__ == "__main__":
     print("Just a test file, run in script mode")
